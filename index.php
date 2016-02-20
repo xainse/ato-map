2fba46d0804e79a6d6df5bc2fc78f31e94652ae9
Serg Xain <xainSE@gmail.com>
2016-02-20 22:57:58 +0200

    add favicon

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!doctype html>
 <html>
-<head>	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title>Карта зони АТО по інформації РНБО України</title>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="shortcut icon" href="img/app/favicon.ico" />
+<title>Карта зони АТО по інформації РНБО України</title>
+
 </head>
 <body>
 <!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34162440 = new Ya.Metrika({ id:34162440, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/34162440" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
@@ -14,7 +16,7 @@
 				<input type="text" value="" id="calendar-date">
 			</span>
 			&copy;&nbsp;2015<?php $y=date('Y');$y!='2015'?'&mdash;'.$y:''?>, <a href="https://github.com/xainse/ato-map" target="_blank">@xainse</a>&nbsp;
-			<a title="ІНФОРМАЦІЙНО-АНАЛІТИЧНИЙ ЦЕНТР - НАЦІОНАЛЬНОЇ БЕЗПЕКИ УКРАЇНИ" href="http://mediarnbo.org/" target="_blank">www.mediarnbo.org</a>
+			<a title="ІНФОРМАЦІЙНО-АНАЛІТИЧНИЙ ЦЕНТР - НАЦІОНАЛЬНОЇ БЕЗПЕКИ УКРАЇНИ" href="http://mediarnbo.org/category/photo-video-uk/" target="_blank">www.mediarnbo.org</a>
 		</div>
 	</header>
 	<div class="header-placeholder"></div>
